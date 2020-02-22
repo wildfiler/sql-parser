10e1bf0b45466d0a5f6d91fdd9d7353b602e9c76
Miquel Barba <miquel.barba@gmail.com>
2020-02-22 11:48:26 +0100

    Fix named vars in LIKE

--- lib/sql-parser/sql_visitor.rb
+++ lib/sql-parser/sql_visitor.rb
@@ -146,8 +146,8 @@ module SQLParser
     end
 
     def visit_Like(o)
-      variable_name = /:\w+/.match(o.right.value)[0]
-      o.right.value = variable_name if variable_name
+      variable_name = /:\w+/.match(o.right.value)
+      o.right.value = variable_name[0] if variable_name
 
       if @negated
         comparison('NOT LIKE', o)
